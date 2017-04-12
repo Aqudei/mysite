40346d6574e1135e3afd5ce85c2ac303e9f92e93
Aqudei <yodi_cute07@yahoo.com>
2017-04-12 08:33:23 +0800

    added profile

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from portfolio.views import portfolio
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^portfolio/', include('portfolio.urls')),
+    url(r'^$', portfolio),
 ]
